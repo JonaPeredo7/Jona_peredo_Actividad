bb8939e9dc6c34b1eae76b8d15f48534a9a9e7ff
JonaPeredo7 <jonathn.peredo.13@gmail.com>
2023-06-02 20:53:03 -0300

    ...

--- app/src/main/java/com/example/jona/MainActivity3.java
+++ app/src/main/java/com/example/jona/MainActivity3.java
@@ -10,41 +10,56 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity
+{
 
     Button btnTelefono, btnGmail;
-    String url = "https://github.com/JonaPeredo7";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        referenciaViews();
-    }
-
-    public void referenciaViews() {
-        btnTelefono = findViewById(R.id.btnTelefono);
         btnGmail = findViewById(R.id.btnGmail);
+        btnGmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {EmailEnviado();}
+        });
 
+
+
+        btnTelefono = findViewById(R.id.btnTelefono);
         btnTelefono.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              Intent intent = new Intent(Intent.ACTION_CALL);
-              intent.setData(Uri.parse("tel:+542954320028"));
-              startActivity(intent);
-            }
-
-        });
-
-        btnGmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-             URI _link = Uri.parse(url);
-             Intent i =  new Intent(Intent.ACTION_VIEW, _link);
-                 startActivity(i);
+                String numeroTelefono = "+542954320028";
+                dialLlamarProgramdor(numeroTelefono);
             }
         });
+
     }
 
+    public void EmailEnviado() {
+        String[] recipient = {"jonathn.peredo.13@gmail.com"};
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" + Uri.encode(recipient[0])));
+        intent.putExtra(Intent.EXTRA_EMAIL, recipient);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(Intent.createChooser(intent, "Enviar correo en: "));
+    }
+
+    private void dialLlamarProgramdor(String numeroTelefono){
+        Uri uri = Uri.parse("tel:" + numeroTelefono);
+        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+
+        if(intent.resolveActivity(getPackageManager()) !=  null){
+            startActivity(intent);
+        }
+    }
 }
+
+
+
